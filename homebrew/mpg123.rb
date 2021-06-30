8deec1b855f371b613f860a220a285fd4856174e
Tom Black <blacktm@gmail.com>
2021-06-30 14:41:26 -0500

    Update Homebrew formulas to latest version

--- homebrew/mpg123.rb
+++ homebrew/mpg123.rb
@@ -2,25 +2,43 @@
 #   https://github.com/Homebrew/homebrew-core/blob/master/Formula/mpg123.rb
 #
 # Changes:
+#   - Added `revision 99` so Homebrew doesn't upgrade the formula
 #   - Added `--enable-static`
 #   - Removed bottles
 
 class Mpg123 < Formula
   desc "MP3 player for Linux and UNIX"
   homepage "https://www.mpg123.de/"
-  url "https://downloads.sourceforge.net/project/mpg123/mpg123/1.25.13/mpg123-1.25.13.tar.bz2"
-  sha256 "90306848359c793fd43b9906e52201df18775742dc3c81c06ab67a806509890a"
+  url "https://www.mpg123.de/download/mpg123-1.28.0.tar.bz2"
+  mirror "https://downloads.sourceforge.net/project/mpg123/mpg123/1.28.0/mpg123-1.28.0.tar.bz2"
+  sha256 "e49466853685026da5d113dc7ff026b1b2ad0b57d78df693a446add9db88a7d5"
+  license "LGPL-2.1-only"
+  revision 99
+
+  livecheck do
+    url "https://www.mpg123.de/download/"
+    regex(/href=.*?mpg123[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
 
   def install
     args = %W[
       --disable-debug
       --disable-dependency-tracking
-      --enable-static
       --prefix=#{prefix}
-      --with-default-audio=coreaudio
       --with-module-suffix=.so
-      --with-cpu=x86-64
+      --enable-static
     ]
+
+    on_macos do
+      args << "--with-default-audio=coreaudio"
+    end
+
+    args << if Hardware::CPU.arm?
+      "--with-cpu=aarch64"
+    else
+      "--with-cpu=x86-64"
+    end
+
     system "./configure", *args
     system "make", "install"
   end
